2df2e75d6f456a83f97836a778f9a3f702d5b000
bu3alwa <ali.bu3alwa@gmail.com>
2022-02-04 10:17:04 -0500

    intial commit

---
+++ src/app/list-pokemon/list.pokemon.components.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "app-list-pokemon",
+    templateUrl: "./list.pokemon.components.html",
+    styleUrls: ["./list.pokemon.components.scss"]
+})
+
+export class ListPokemonComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
